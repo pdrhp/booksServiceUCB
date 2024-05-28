2e83c3550685d8e24bc09bbede7e2ba6f195827f
pdrhp <pedrohsp2004@gmail.com>
2024-05-28 18:16:14 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+
+
+app.use(express.json());
+
+app.get('/books', (req, res) => {
+    
+});
+
+
+app.post('/books', (req, res) => {
+    
+})
+
+app.post('/books/comprar')
+
+
+app.listen(3000, () => {
+    console.log('Aplicação iniciada na porta 3000');
+});
---
+++ models/Book.js
@@ -0,0 +1,10 @@
+export class Livro {
+    constructor(id,titulo, autor, genero, imagem, quantidade) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.imagem = imagem;
+        this.quantidade = quantidade;
+    }
+}
